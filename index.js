fb36a81f035fce238536ae9fa7ed0d0f09b611f7
vishakp <vishakpanthalath@gmail.com>
2019-03-23 13:55:33 +0530

    assembler function created

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-// Import stylesheets
-import './style.css';
+let RobotMaker = ()=>{
+  let self = {};
+  self.models = {'base': null,'body':null,'arm1':null,'arm2':null,'hand':null}
+  self.init = ()=>{
+    self.scene = new THREE.Scene();
+    self.light = new THREE.AmbientLight(0x333333)
+    self.camera = new THREE.PerspectiveCamera(60,window.innerHeight/window.innerHeight,1,15000);
+    self.renderer = new THREE.WebGLRenderer({antialias: true});
+    self.loader = new THREE.ObjectLoader()
+  }
+  self.init()
+  self.scene.add(self.light)
+  self.camera.position = {x:0, y:0, z:7000}
+  self.renderer.setSize(window.innerWidth, window.innerHeight);
+  document.getElementById('robot').appendChild(self.renderer.domElement)
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+  let assembler = (name) =>{
+    return (geometry) =>{
+      self.assembler(name, geometry)
+    }
+  }
+  for(let part in self.models){
+    self.loader.load( 'js/robot/robort_arm_'+part+'.js',assembler(part))
+  }
+  // self.update()
+
+  return self;
+}
+
+
+
+
+let robot = RobotMaker();
